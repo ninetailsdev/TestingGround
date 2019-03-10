14e56ad93e0ba3b25484aa877a2de764832f8fe0
Sean O'Brien <ninetailsdevelopment@gmail.com>
2019-03-10 16:19:08 +0000

    added in bootstrap and a few more pages

--- DutchTreat/DutchTreat/Controllers/AppController.cs
+++ DutchTreat/DutchTreat/Controllers/AppController.cs
@@ -2,14 +2,53 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DutchTreat.Models;
+using DutchTreat.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DutchTreat.Controllers
 {
     public class AppController : Controller
     {
+        public readonly IMailService _mailService;
+
+        public AppController(IMailService mailService)
+        {
+            _mailService = mailService;
+        }
+
         public IActionResult Index()
         {
+            return View();
+        }
+
+        [HttpGet("contact")]
+        public IActionResult Contact()
+        {
+           
+            return View();
+        }
+
+        [HttpPost("contact")]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // send message
+                _mailService.SendMessage("EmailAddress", model.Subject, $"From:{model.Name} - {model.Email}, Message: {model.Message}");
+
+                ViewBag.UserMessage = "mail sent";
+
+                ModelState.Clear();
+            }
+          
+
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewBag.Title = "About Us";
             return View();
         }
     }
--- DutchTreat/DutchTreat/Program.cs
+++ DutchTreat/DutchTreat/Program.cs
@@ -20,5 +20,7 @@ namespace DutchTreat
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
+
+
     }
 }
