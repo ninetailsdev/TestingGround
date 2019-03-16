9903b7c7269a15aa35d6b75e89a01762d195e4dd
Sean O'Brien <ninetailsdevelopment@gmail.com>
2019-03-16 10:51:58 +0000

    finished off the shop page and put in some basic logging

--- DutchTreat/DutchTreat/Data/DutchRepository.cs
+++ DutchTreat/DutchTreat/Data/DutchRepository.cs
@@ -1,4 +1,5 @@
 ﻿using DutchTreat.Data.Entities;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,27 +10,59 @@ namespace DutchTreat.Data
     public class DutchRepository : IDutchRepository
     {
         private readonly DutchContext _ctx;
+        private readonly ILogger<DutchRepository> _logger;
 
-        public DutchRepository(DutchContext ctx)
+        public DutchRepository(DutchContext ctx, ILogger<DutchRepository> logger)
         {
             _ctx = ctx;
+            _logger = logger;
         }
 
         public IEnumerable<Product> GetAllProducts()
         {
-            return _ctx.Products.OrderBy(p => p.Title).ToList();
+            try
+            {
+                _logger.LogInformation("Getting all products");
+                return _ctx.Products.OrderBy(p => p.Title).ToList();
+            }
+            catch(Exception ex)
+            {
+                _logger.LogError($"Failed to get all products: {ex}");
+                return null;
+            }
+          
         }
 
         public IEnumerable<Product> GetProductsByCategory(string category)
         {
-            return _ctx.Products
-                       .Where(p => p.Category == category)
-                       .ToList();
+            try
+            {
+                _logger.LogInformation("Getting all products by category");
+                return _ctx.Products
+                    .Where(p => p.Category == category)
+                    .ToList();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Failed to get all products by category: {ex}");
+                return null;
+            }
+         
         }
 
         public bool SaveAll()
         {
-            return _ctx.SaveChanges() > 0;
+            try
+            {
+                _logger.LogInformation($"saving everything");
+                return _ctx.SaveChanges() > 0;
+            }
+            catch(Exception ex)
+            {
+                _logger.LogError($"Failed to save everything: {ex}");
+                return false;
+            }
+          
         }
     }
 }
