ff32bbd7a61c99d2f04cfd2b5509e29494369012
Sean O'Brien <ninetailsdevelopment@gmail.com>
2019-03-31 14:45:26 +0100

    angular stuff

---
+++ DutchTreat/DutchTreat/wwwroot/clientapp/out-tsc/ClientApp/app/app.component.js
@@ -0,0 +1,17 @@
+import * as tslib_1 from "tslib";
+import { Component } from '@angular/core';
+var AppComponent = /** @class */ (function () {
+    function AppComponent() {
+        this.title = 'Dutch Treat';
+    }
+    AppComponent = tslib_1.__decorate([
+        Component({
+            selector: 'the-shop',
+            template: "\n    <!--The content below is only a placeholder and can be replaced.-->\n    <div style=\"text-align:center\">\n      <h1>\n        Welcome to {{title}}!\n      </h1>\n    </div>\n  ",
+            styles: []
+        })
+    ], AppComponent);
+    return AppComponent;
+}());
+export { AppComponent };
+//# sourceMappingURL=app.component.js.map
